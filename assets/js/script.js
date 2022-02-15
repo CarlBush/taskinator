28c43e8bc9d90957eaac63f91f4c2993b8ba2b63
CarlBush <Carl.BushS13@gmail.com>
2022-02-15 14:16:51 -0700

    innerhtml added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,27 @@ var formEl = document.querySelector("#task-form");
 
 var createTaskHandler = function() {
 event.preventDefault();
+var taskNameInput = document.querySelector("input[name='task-name']").value;
+var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+//1. Creating the element "Li"
+//2. Linking the class name for style
+//3. Retriving the actual data first from varible "taskNameInput" then from the value
+//4. Adding the data to site per child->parent
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    listItemEl.textContent = taskNameInput; //this might be able to be removed
+    tasksToDoEl.appendChild(listItemEl); //this might be able to be removed//
+
+//1. Creating the element "div"
+//2. Linking the class name for style
+//3. Adding HTML to the div
+//4. Adding the data to Li 
+//5. Adding the data to the div
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
